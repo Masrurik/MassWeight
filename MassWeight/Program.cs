f73af34097b0b6d7de15d70fbc7892b23fbef780
Masrur Sadiev <masrur.sadiev@outlook.com>
2021-03-18 09:36:23 -0400

    implementing Mass and Weight

---
+++ MassWeight/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MassWeight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Please enter a mass");
+
+            double mass = Convert.ToDouble(Console.ReadLine());
+            double wheight = mass * 9.8;
+            if (wheight > 1000)
+            {
+                Console.Write("To heavy");
+            }
+            else if(wheight < 10)
+            {
+                Console.Write("To light");
+            }
+        }
+    }
+}
